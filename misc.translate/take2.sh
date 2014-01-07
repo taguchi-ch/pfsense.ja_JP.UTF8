35a4d79c4ab0464409587e5976758ae995dbe3e2
Isaac (.ike) Levy <ike@blackskyresearch.net>
2014-01-06 22:05:57 -0500

    getting hacky with machine translation of gettext .po files
    Signed-off-by: Isaac (.ike) Levy <ike@blackskyresearch.net>

---
+++ misc.translate/take2.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# machine translation for pfSense gettext .po files using google translate
+# compared to awk(1), this is REALLY slow in shell, but way easier to work with.
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+_translate() {
+# function to translate from raw line
+echo "${*}"
+}
+
+## vars
+
+TARGETLANG="${2:-jp}"
+
+## action
+
+# print .po "header", (email style)
+try sed '/^$/q' "$1"
+
+####################
+# line by line, everything after the "header", (email style)
+sed '1,/^$/d' "$1" |\
+while read line ; do
+
+#while line is still quoted, strip leading/trailing whitespace,
+_i="`echo ${line} | try sed 's/^[ \t]*//'`"
+
+# if line is empty, print it
+echo "${_i}" | grep -v '.'
+# if line is comment, '^#', print it
+echo "${_i}" | grep '^#'
+
+if echo "${_i}" | grep -lq '^msgid' ; then
+   _src=1
+    echo "${_i}"
+   _transline=""
+   _transline="${_transline}${_i}"
+elif echo "${_i}" | grep -lq '^msgstr' ; then
+  _src=0
+  # TRANSLATE '_transline'
+  echo "msgstr \"${_transline}\""
+elif echo "${_i}" | grep -lq '^"' ; then
+  if [ ${_src} = 1 ] ; then
+    echo "${_i}"
+    _transline="${_transline}${_i}"
+  fi
+fi
+
+# awk ' /msgid/ {flag=1;next} /msgstr/{flag=0} flag { print }' ./usr/local/share/locale/ja_JP.UT
+# F8/LC_MESSAGES/pfSense.machine.po |\
+# sed 's/^"//;s/"$//;s/\[/((/g;s/\]/))/g;s/%s/foo.s/g' |\
+# sed 's/\!/\\\!/g;s/\$/\\\$/g;s/\`/\\\`/g;s/\\/\\\\/g;' |\
+# while read line ; do
+# echo "$line" | sed 's/((/\[/g;s/))/\]/g'
+# trs {en=ja} "$line" | sed 's/((/\[/g;s/))/\]/g'
+# echo '--'
+# sleep .25
+# done
+
+
+#done < "$1"
+done
+###########
+
+true
